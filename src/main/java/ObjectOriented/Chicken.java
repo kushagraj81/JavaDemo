55ee36f901ccd087c09f6b920ac94dd77003282f
Kushagra <kushagraj81@gmail.com>
2024-01-21 15:49:31 +0530

    add the use of method override

--- src/main/java/ObjectOriented/Chicken.java
+++ src/main/java/ObjectOriented/Chicken.java
@@ -6,6 +6,11 @@ public class Chicken extends Bird{
         super(age,weight);
 
     }
+//    Use it every time you override a method for two benefits.
+//    Do it so that you can take advantage of the compiler checking to make sure you actually are overriding a method when you think you are.
+//    This way, if you make a common mistake of misspelling a method name or not correctly matching the parameters,
+//    you will be warned that you method does not actually override as you think it does. Secondly,\
+//    it makes your code easier to understand because it is more obvious when methods are overwritten.
     @Override
     public void fly()
     {

ad91d39cc5117b451443ab105f69aea4de49d01a
Kushagra <kushagraj81@gmail.com>
2024-01-21 14:44:15 +0530

    Static variable explanation

---
+++ src/main/java/Foundation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import Javaloops.Loops;
+public class Foundation{
+    public static void main(String args[])
+    {
+        System.out.println("hello kushagra");
+        Loops obj = new Loops();
+        obj.printCount();
+        Loops obj2 = new Loops();
+        obj2.printCount();
+    }
+
+}
---
+++ src/main/java/Javaloops/Loops.java
@@ -0,0 +1,18 @@
+package Javaloops;
+
+public class Loops {
+    private static Integer count=0;
+   public Loops()
+    {
+        System.out.println("inside Loops class");
+    }
+    public void printCount()
+    {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(count);
+            count+=2;
+        }
+    }
+}
+
